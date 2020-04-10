a6f7211857f18e1a63923a7eed36b872550dcb87
kkyueing <kkyueing@gmail.com>
2020-04-10 14:53:40 +0000

    전수 데이터 분석

--- cli.py
+++ cli.py
@@ -21,27 +21,43 @@ FIELDS = [
 
 class CLI:
     """News analyzer"""
+    def tag_and_stats(self):
+        articles = list(self._prepare())
+        self._tag(articles)
+        self._stats(articles)
+
     def tag(self):
         """기사 전체를 분석하여 분류 태그를 추가한 후 별도 파일로 저장"""
-        overrides = self._load_override_rules()
-        articles = self._analyze_articles(self._get_articles(), overrides)
-        os.makedirs(DATA_DIR, exist_ok=True)
-        with open(os.path.join(DATA_DIR, 'articles.csv'), 'w') as f:
-            csvw = csv.DictWriter(f, FIELDS)
-            csvw.writeheader()
-            for article in articles:
-                if len(article['tags']) == 0:
-                    continue
-                article['tags'] = ';'.join(article['tags'])
-                csvw.writerow(article)
+        articles = self._prepare()
+        self._tag(articles)
 
     def stats(self):
         """통계분석"""
-        overrides = self._load_override_rules()
-        articles = self._analyze_articles(self._get_articles(), overrides)
-        os.makedirs(DATA_DIR, exist_ok=True)
+        articles = self._prepare()
+        self._stats(articles)
 
+    def _tag(self, articles):
+        with open(os.path.join(DATA_DIR, 'articles.csv'), 'w') as f:
+            n_tagged = 0
+            csvw = csv.DictWriter(f, FIELDS)
+            csvw.writeheader()
+            for i, article in enumerate(articles):
+                if i >= 50000 and i % 50000 == 0:
+                    print(f'Tagged {n_tagged}/{i} articles')
+
+                if len(article['tags']) == 0:
+                    continue
+                else:
+                    n_tagged += 1
+
+                csvw.writerow({
+                    **article,
+                    'tags': ';'.join(article['tags']),
+                })
+
+    def _stats(self, articles):
         # 일별/언론별/분류별 빈도 집계
+        print('Creating stats.csv...')
         table = stats.aggregate_by_date_cp_tag(articles)
         with open(os.path.join(DATA_DIR, 'stats.csv'), 'w') as f:
             fields = ['date', 'cp_name', 'tag', 'count']
@@ -49,13 +65,9 @@ class CLI:
             csvw.writeheader()
             csvw.writerows(table)
 
-        # 최근 6개월 이내 데이터만 남기기
-        latest = datetime.strptime(max(t['date'] for t in table), '%Y%m%d')
-        window = (latest - timedelta(days=180)).strftime('%Y%m%d')
-        recent_table = [t for t in table if t['date'] >= window]
-
         # 일별 집계
-        daily = stats.daily_articles(recent_table)
+        print('Creating stats_daily.csv...')
+        daily = stats.daily_articles(table)
         with open(os.path.join(DATA_DIR, 'stats_daily.csv'), 'w') as f:
             fields = ['date', 'clean', 'bad', 'total', 'ratio', 'z']
             csvw = csv.DictWriter(f, fields)
@@ -63,7 +75,8 @@ class CLI:
             csvw.writerows(daily)
 
         # 최근 6개월 이내에 가장 빈도가 높은 태그 집계
-        freq_tags = stats.frequent_tags(recent_table)
+        print('Creating stats_freq_tags.csv...')
+        freq_tags = stats.frequent_tags(table)
         with open(os.path.join(DATA_DIR, 'stats_freq_tags.csv'), 'w') as f:
             fields = ['tag', 'count', 'total', 'ratio']
             csvw = csv.DictWriter(f, fields)
@@ -71,8 +84,9 @@ class CLI:
             csvw.writerows(freq_tags)
 
         # 최근 6개월 이내에 가장 부적절한 표현이 담긴 기사의 비율이 낮은
-        # 언론사 집계 (단, 최근 6개월 이내에 기사가 50개 이상인 경우만)
-        best_cps = stats.best_cps(recent_table, min_count=50)
+        # 언론사 집계 (단, 최근 6개월 이내에 기사가 200개 이상인 경우만)
+        print('Creating stats_best_tags.csv...')
+        best_cps = stats.best_cps(table, min_count=200)
         with open(os.path.join(DATA_DIR, 'stats_best_cps.csv'), 'w') as f:
             fields = ['cp_name', 'cp_name_masked', 'clean', 'bad', 'total',
                       'ratio']
@@ -82,8 +96,9 @@ class CLI:
                            for c in best_cps)
 
         # 최근 6개월 이내에 가장 부적절한 표현이 담긴 기사의 비율이 높은
-        # 언론사 집계 (단, 최근 6개월 이내에 기사가 50개 이상인 경우만)
-        worst_cps = stats.worst_cps(recent_table, min_count=50)
+        # 언론사 집계 (단, 최근 6개월 이내에 기사가 200개 이상인 경우만)
+        print('Creating stats_worst_tags.csv...')
+        worst_cps = stats.worst_cps(table, min_count=200)
         with open(os.path.join(DATA_DIR, 'stats_worst_cps.csv'), 'w') as f:
             fields = ['cp_name', 'cp_name_masked', 'clean', 'bad', 'total',
                       'ratio']
@@ -94,17 +109,25 @@ class CLI:
 
     def test(self, tag):
         """기사 전체 중 특정 분류에 속하는 기사만 출력. 개발 중 테스트용"""
-        articles = (
-            a for a in self._analyze_articles(self._get_articles())
-            if tag in a['tags']
-        )
+        articles = (a for a in self._prepare() if tag in a['tags'])
         csvw = csv.DictWriter(sys.stdout, FIELDS)
         csvw.writeheader()
         csvw.writerows(articles)
 
+    def _prepare(self):
+        overrides = self._load_override_rules()
+        articles = self._analyze_articles(self._get_articles(), overrides)
+        os.makedirs(DATA_DIR, exist_ok=True)
+        return articles
+
+
     def _get_articles(self):
         """Yields all articles in SRC_DIR"""
-        for filename in sorted(os.listdir(SRC_DIR)):
+        filenames = (
+            fn for fn in sorted(os.listdir(SRC_DIR))
+            if fn.endswith('.csv')
+        )
+        for filename in filenames:
             date = filename[:-4]
             with open(os.path.join(SRC_DIR, filename), 'r') as f:
                 csvr = csv.DictReader(f)
