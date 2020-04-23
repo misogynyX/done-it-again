469a727bb08c449088cd2125d4947e94270d7e27
kkyueing <kkyueing@gmail.com>
2020-04-23 09:54:44 +0000

    fix: 깃헙 액션 수정

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 mkdir -p _data/
 
 echo "Downloading files from S3..."
-aws s3 sync --quiet _data/ s3://${AWS_S3_BUCKET}/done-it-again-analysis/
+aws s3 sync --quiet s3://${AWS_S3_BUCKET}/done-it-again-analysis/ _data/
