b2a313ef0aedd59727c7bc4b2bd657c37e2d6217
kkyueing <kkyueing@gmail.com>
2020-04-23 09:47:59 +0000

    refactor: 분석 관련 코드 분리
    분석 관련 코드는 done-it-again-analysis 저장소로 분리했습니다.

--- update.sh
+++ update.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p data
+mkdir -p _data/
 
-echo "Downloading recent files from S3..."
-recent_files=`aws s3 ls ${AWS_S3_BUCKET}/news/ | awk '{print $4}' | sort | tail -n 180`
-while IFS= read -r line; do
-  if [ ! -f data/${line} ]; then
-    aws s3 cp --no-progress s3://${AWS_S3_BUCKET}/news/${line} data/ &
-    sleep 0.5
-  fi
-done <<< "${recent_files}"
-wait
-
-echo "Unzipping files..."
-gunzip -kf data/*.csv.gz
-
-echo "Analyzing..."
-python cli.py tag_and_stats
+echo "Downloading files from S3..."
+aws s3 sync --quiet _data/ s3://${AWS_S3_BUCKET}/done-it-again-analysis/
