adc18d0ec523506850afafa61b1ae63e3e873814
kkyueing <kkyueing@gmail.com>
2020-04-23 10:14:04 +0000

    fix: 소소한 수정

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 mkdir -p docs/_data/
 
 echo "Downloading files from S3..."
-aws s3 sync --quiet s3://${AWS_S3_BUCKET}/done-it-again-analysis/ docs/_data/
+aws s3 sync s3://${AWS_S3_BUCKET}/done-it-again-analysis/ docs/_data/
